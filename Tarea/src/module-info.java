65916e197d139b0d5af0d9d0c1f96af381e25c09
unknown <jsbaidal@espol.edu.ec>
2023-10-27 18:46:54 -1100

    base de proyecto

---
+++ Tarea/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Tarea {
+	requires java.desktop;
+}
---
+++ Tarea/src/tarea/PanePrincipal.java
@@ -0,0 +1,86 @@
+package tarea;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class PanePrincipal extends JFrame {
+	private JFrame ventana;
+	private JButton botonRojo;
+	private JButton botonVerde;
+	private JButton botonAzul;
+	private JPanel ventanaContenedora;
+	
+	public PanePrincipal() {
+	 ventana= new JFrame();
+	 ventana.setVisible(true);
+	 ventana.setBounds(200, 200, 500, 500);
+	 ventana.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	 ventana.setTitle("Programa Principal");
+	 
+	 ventanaContenedora = new JPanel();
+	 ventanaContenedora.setBorder(new EmptyBorder(5, 5, 5, 5));
+	 
+	 ventana.setContentPane(ventanaContenedora);
+	 
+	 botonRojo = new JButton("Rojo");
+	 botonVerde = new JButton("Verde");
+	 botonAzul = new JButton("Azul");
+	 
+	 
+	 
+	 
+	 
+	 ventanaContenedora.add(botonRojo);
+	 ventanaContenedora.add(botonAzul);
+	 ventanaContenedora.add(botonVerde);
+	 
+	 botonRojo.addActionListener( new ActionListener() {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			ventanaContenedora.setBackground(Color.red);
+			System.out.println("Color Rojo");
+		}
+		 
+	 }	 
+	);
+	 
+	 
+	 botonVerde.addActionListener( new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ventanaContenedora.setBackground(Color.green);
+				System.out.println("Color Verde");
+			}
+			 
+		 }	 
+		);
+	 
+	 botonAzul.addActionListener( new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ventanaContenedora.setBackground(Color.blue);
+				System.out.println("Color Azul");
+			}
+			 
+		 }	 
+		);
+	 
+	 
+	 
+	 
+	}
+	
+	
+	
+	
+	
+}
